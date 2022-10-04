1e17e874018f6f2d06ef623ecc0d05dafffc5bf4
solomon <hansolomon168@gmail.com>
2022-10-04 13:22:12 +0600

    feat: finished swap_diagonales

--- lab5/swap_diagonals.py
+++ lab5/swap_diagonals.py
@@ -6,7 +6,22 @@
 """
 
 
-def main():
-    matrix_size = 3
-    for row in range(matrix_size):
-        list = []
+def swap(a):
+    n = len(a)
+    for i in range(n):
+        a[i][i], a[n-i-1][i] = a[n-i-1][i], a[i][i]
+
+
+def show(a):
+    for r in a:
+        print (', '.join(['{:2d}'.format(i) for i in r]))
+
+print('Введите размерность матрицы: ')
+matrix_size = int(input())
+
+a = [[j + i * matrix_size + 1 for j in range(matrix_size)] for i in range(matrix_size)]
+
+show(a)
+swap(a)
+print()
+show(a)
